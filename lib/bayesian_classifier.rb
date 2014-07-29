63db4a9f3f18097a954f3cac7dbfcad78c88d05f
Prashant <prashant@urbanzeak.com>
2014-07-29 19:09:27 +0530

    Changed the code so that there is a puts showing at what stage the code is

--- lib/bayesian_classifier.rb
+++ lib/bayesian_classifier.rb
@@ -6,34 +6,38 @@ class BayesianClassifier
   def process_all_unprocessed_leads
     bayes_classifier = StuffClassifier::Bayes.new("Leads or Nonleads")
 
+    puts "Beginning training"
+
     training_leads = TrainingLead.all
     training_leads.each do |training_lead|
       bayes_classifier.train(:lead, training_lead.tweet_body)
     end
-
+    puts "Finished positive training"
+    puts "Beginning negative training"
     non_leads = TrainingNonLead.all
     non_leads.each do |training_nonlead|
       bayes_classifier.train(:nonlead, training_nonlead.tweet_body)
     end
-
+    puts "Finished negative training"
     unprocessed_leads = UnprocessedLead.all
+
+    processed_count = 1
 
     unprocessed_leads.each do |unprocessed_lead|
       if UrlChecker.does_not_contains_url?(unprocessed_lead.tweet_body)
         if LocationChecker.preferred_location_available?(unprocessed_lead.user_location)
           if bayes_classifier.classify(unprocessed_lead.tweet_body) == :lead
             Lead.create(tweet_poster_screen_name: unprocessed_lead.tweet_poster_screen_name, tweet_user_image: unprocessed_lead.tweet_user_image, tweet_body: unprocessed_lead.tweet_body, gnip_matching_rules: unprocessed_lead.gnip_matching_rules, user_location: unprocessed_lead.user_location)
-            unprocessed_lead.destroy
-          else
-            unprocessed_lead.destroy
           end
-        else
-          unprocessed_lead.destroy
         end
-      else
-        unprocessed_lead.destroy
       end
+
+      unprocessed_lead.destroy
+      puts "The processed count is: #{processed_count}"
+      processed_count = processed_count + 1
     end
+
+
   end
 
   handle_asynchronously :process_all_unprocessed_leads

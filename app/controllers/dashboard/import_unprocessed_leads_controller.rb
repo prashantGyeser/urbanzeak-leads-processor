f0a76472c75185196f9a64aec4e98584a9225ea9
Prashant <prashant@urbanzeak.com>
2014-07-29 14:51:03 +0530

    Changed the structure of the delayed job

--- app/controllers/dashboard/import_unprocessed_leads_controller.rb
+++ app/controllers/dashboard/import_unprocessed_leads_controller.rb
@@ -5,22 +5,8 @@ require 'csv_importer'
 
 class Dashboard::ImportUnprocessedLeadsController < Dashboard::ApplicationController
   def s3_import
-
-    # retrieve the access key and secret key
-    access_key_id = 'AKIAJGCSGA4PHLWO7JJA'
-    secret_access_key = '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'
-
-    # create an instance of the s3 client
-    s3 = AWS::S3.new(access_key_id: access_key_id, secret_access_key: secret_access_key)
-
-    # get the bucket
-    bucket = s3.buckets['gnip_cleaned_output']
-
-    # retrieve the objects
-    bucket.objects.each do |object|
-
-      CsvImporter.new.delay.save_rows_in_file_to_database(object)
-    end
+    csv_importer = CsvImporter.new
+    csv_importer.save_rows_in_file_to_database
   end
 end
 
--- lib/csv_importer.rb
+++ lib/csv_importer.rb
@@ -1,18 +1,30 @@
 require 'data_parser'
 
 class CsvImporter
-  def save_rows_in_file_to_database(object_reference)
-    csv_file = object_reference.read
-    count = 1
-    rows = CSV.parse(csv_file)
-    rows.each do |row|
-      puts count
-      count = count +1
-      tweet_hash = DataParser.convert_row_into_hash(row)
-      UnprocessedLead.create(tweet_hash)
+  def save_rows_in_file_to_database
+    # retrieve the access key and secret key
+    access_key_id = 'AKIAJGCSGA4PHLWO7JJA'
+    secret_access_key = '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'
+
+    # create an instance of the s3 client
+    s3 = AWS::S3.new(access_key_id: access_key_id, secret_access_key: secret_access_key)
+
+    # get the bucket
+    bucket = s3.buckets['gnip_cleaned_output']
+
+    # retrieve the objects
+    bucket.objects.each do |object|
+      csv_file = object.read
+      count = 1
+      #CsvImporter.new.delay.save_rows_in_file_to_database(object)
+      rows = CSV.parse(csv_file)
+      rows.each do |row|
+        count = count +1
+        tweet_hash = DataParser.convert_row_into_hash(row)
+        UnprocessedLead.create(tweet_hash)
+      end
     end
-
-
-
   end
+
+  handle_asynchronously :save_rows_in_file_to_database
 end

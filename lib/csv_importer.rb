a2e0dd38f6a1036c8df91db6ca9f5902fd13684e
Prashant <prashant@urbanzeak.com>
2014-07-31 07:04:35 +0530

    Went back tot he old code for the storing code

--- lib/csv_importer.rb
+++ lib/csv_importer.rb
@@ -3,6 +3,7 @@ require 'csv'
 
 class CsvImporter
   def save_rows_in_file_to_database
+    puts "It has started the s3 import"
     # retrieve the access key and secret key
     access_key_id = 'AKIAJGCSGA4PHLWO7JJA'
     secret_access_key = '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'
@@ -13,22 +14,31 @@ class CsvImporter
     # get the bucket
     bucket = s3.buckets['gnip_cleaned_output']
     # retrieve the objects
+    puts "Before iterating over the objects in amazon"
     bucket.objects.each do |object|
       csv_file = object.read
       count = 1
       malformed_count = 1
       #CsvImporter.new.delay.save_rows_in_file_to_database(object)
+      puts "Before the error loop"
       begin
-        #rows = CSV.parse(csv_file)
-        #rows.each do |row|
-        CSV.foreach(csv_file) do |row|
-          puts "Importing row: #{count}"
-          count = count +1
+        rows = CSV.parse(csv_file)
+        rows.each do |row|
+          puts "Processing row: #{1}"
           tweet_hash = DataParser.convert_row_into_hash(row)
-          UnprocessedLead.create(tweet_poster_screen_name: tweet_hash[:tweet_poster_screen_name], tweet_user_image: tweet_hash[:tweet_user_image], tweet_body: tweet_hash[:tweet_body], gnip_matching_rules: tweet_hash[:gnip_matching_rules], user_location: tweet_hash[:user_location] )
-
-
+          UnprocessedLead.create(tweet_hash)
         end
+
+
+        #rows.each do |row|
+        # CSV.foreach(csv_file) do |row|
+        #   puts "Importing row: #{count}"
+        #   count = count +1
+        #   tweet_hash = DataParser.convert_row_into_hash(row)
+        #   UnprocessedLead.create(tweet_poster_screen_name: tweet_hash[:tweet_poster_screen_name], tweet_user_image: tweet_hash[:tweet_user_image], tweet_body: tweet_hash[:tweet_body], gnip_matching_rules: tweet_hash[:gnip_matching_rules], user_location: tweet_hash[:user_location] )
+        #
+        #
+        # end
       rescue CSV::MalformedCSVError
         puts "Malformed csv row: #{malformed_count}"
         malformed_count = malformed_count + 1

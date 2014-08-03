3a1b5a8bc5c6fa246b9686204858c043b6563ed7
Prashant <prashant@gyeser.com>
2014-08-03 16:22:42 -0700

    Added to the list of cities to look in for the tweets

--- lib/location_checker.rb
+++ lib/location_checker.rb
@@ -1,6 +1,6 @@
 class LocationChecker
   def self.preferred_location_available?(tweet_location)
-    preferred_location_array = ['nyc', 'new york', 'manhattan', 'queens', 'long island', 'hoboken', 'brooklyn', 'jersey' ]
+    preferred_location_array = ['nyc', 'new york', 'manhattan', 'queens', 'long island', 'hoboken', 'brooklyn', 'jersey', 'sfo', 'san francisco', 'boston', 'la', 'los angeles', 'pasadena'  ]
     preferred_location_array.each do |location|
       unless tweet_location.nil?
         return true if tweet_location.downcase.include? location.downcase

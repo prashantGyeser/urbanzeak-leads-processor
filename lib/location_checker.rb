df635d1d0e95f4b720048e122395c1faeef6e9e5
Prashant <prashant@gyeser.com>
2014-08-04 11:18:24 -0700

    Changed the city names to make it more general

--- lib/location_checker.rb
+++ lib/location_checker.rb
@@ -1,6 +1,6 @@
 class LocationChecker
   def self.preferred_location_available?(tweet_location)
-    preferred_location_array = ['New York City', 'New York', 'NYC', 'Long Island', 'Jersey City', 'Hoboken', 'Manhattan', 'Queens', 'Bronx', 'Brooklyn', 'Staten Island', 'Harlem', 'Boston', 'Cambridge', 'Newton', 'Watertown',  'Winthrop', 'Revere', 'Chelsea', 'Everett', 'Somerville', 'Brookline','Milton', 'Charlestown', 'Los Angeles', 'LA City', 'Hollywood', 'Wilshire', 'Westside', 'Crescenta Valleys', 'Glendale', 'Pasadena', 'El Monte', 'Downey', 'Inglewood', 'San Francisco', 'SFO', 'Bay Area', 'Mission Bay', 'Hayes Valley', 'Fillmore', 'Noe Valley', 'Bernal Heights', 'Berkley', 'Mill Valley', 'Daly City', 'San Bruno']
+    preferred_location_array = ['nyc', 'new york', 'manhattan', 'queens', 'long island', 'hoboken', 'brooklyn', 'jersey', 'bronx', 'staten island', 'harlem', 'boston', 'cambridge', 'newton', 'watertown', 'winthrop', 'revere', 'chelsea', 'everett', 'somerville', 'brookline','milton', 'charlestown', 'los angeles', 'la city', 'hollywood', 'wilshire', 'westside', 'crescenta valleys', 'glendale', 'pasadena', 'el monte', 'downey', 'inglewood', 'san francisco', 'sfo', 'bay area', 'mission bay', 'hayes valley', 'fillmore', 'noe valley', 'bernal heights', 'berkley', 'mill valley', 'daly city', 'san bruno']
     preferred_location_array.each do |location|
       unless tweet_location.nil?
         return true if tweet_location.downcase.include? location.downcase

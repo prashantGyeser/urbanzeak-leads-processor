c3e33218aaf3a1eaa0127f4e02f0f7de016acb28
Prashant <prashant@gyeser.com>
2014-08-15 07:19:29 -0700

    Added code to create a user when it is created on the front end

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,18 @@
+require 'json'
+
 class UsersController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => :create
   def create
 
-    @ironmq = IronMQ::Client.new()
+    puts "Okay it is getting called"
+    puts params.inspect
 
-    # Get a queue (if it doesn't exist, it will be created when you first post a message)
-    @queue = @ironmq.queue("new_user_queue")
-
-    msg = @queue.get()
-
-    message = msg.body
-
-    message_array = message.split(',')
-
-    user = User.create(email: message_array[0])
-    oauth_token = OauthToken.create(user_id: user.id, oauth_token: message_array[1], oauth_secret: message_array[2], provider: "Twitter")
+    user = User.create(email: params[:email])
+    oauth_token = OauthToken.create(user_id: user.id, oauth_token: params[:oauth_token], oauth_secret: params[:oauth_secret], provider: "twitter")
     UserCity.create(city_id: 1, user_id: user.id)
     UserCategory.create(category_id: 1, user_id: user.id)
 
-
-    # Delete a message (you must delete a message when you're done with it or it will go back on the queue after a timeout)
-    #msg.delete # or @queue.delete(msg.id)
+    render json: user
 
 
   end

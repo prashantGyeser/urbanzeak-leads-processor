46ae67424e1c1844d18897a6cd2140162db636c0
Prashant <prashant@gyeser.com>
2014-08-16 09:20:08 -0700

    Added code to create a user, their categories, cities and tokens when the data sent in is valid

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,17 @@ class UsersController < ApplicationController
     puts "Okay it is getting called"
     puts params.inspect
 
+    city = City.where(city_name: params[:city]).first
+    category = Category.where(name: params[:category]).first
+
     user = User.create(email: params[:email])
     oauth_token = OauthToken.create(user_id: user.id, oauth_token: params[:oauth_token], oauth_secret: params[:oauth_secret], provider: "twitter")
-    UserCity.create(city_id: 1, user_id: user.id)
-    UserCategory.create(category_id: 1, user_id: user.id)
+
+    UserCity.create(city_id: city.id, user_id: user.id)
+    UserCategory.create(category_id: category.id, user_id: user.id)
 
     render json: user
+
 
 
   end

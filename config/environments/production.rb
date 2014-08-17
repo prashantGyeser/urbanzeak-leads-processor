9ec798383c713cf36f6459e502469747e083e20b
Prashant <prashant@gyeser.com>
2014-08-17 10:21:13 -0700

    Changed the production environment file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Adding the lib directory to the autoload path to make sure rake can run
+  config.autoload_paths += %W(#{config.root}/lib)
+
 end

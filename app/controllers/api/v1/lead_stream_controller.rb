04dc724646d1599805cd13fa304f38fce90c94f2
Prashant <prashant@gyeser.com>
2014-10-08 00:13:59 -0700

    Added code to store the unique lead streams from the frontend. Still need to add a rake task on the front end to call this function

--- app/controllers/api/v1/lead_stream_controller.rb
+++ app/controllers/api/v1/lead_stream_controller.rb
@@ -6,8 +6,18 @@ class Api::V1::LeadStreamController < ApplicationController
     array_of_streams = params[:_json]
 
     array_of_streams.each do |stream|
-      puts stream["city"]
+      city = City.find_by_city_name(stream["city"])
+      category = Category.find_by_name(stream["category"])
+
+      puts "The city is: #{city.id}"
+
+      UniqueLeadStream.create(city_id: city.id, category_id: category.id)
     end
+
+    respond_to do |format|
+      format.json { render :json => "Created sucessfully" }
+    end
+
 
   end
 end
